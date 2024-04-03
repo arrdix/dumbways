27be650a07b249a36e020b2f4d1ac9f0ed54b0c0
arrdix <mail.ardix@gmail.com>
2024-04-03 23:29:26 +0700

    feat: implement task search feature for task routes

--- script/main.js
+++ script/main.js
@@ -3,13 +3,45 @@ window.addEventListener("DOMContentLoaded", () => {
 })
 
 function initRoutes() {
-    const routes = document.getElementById("routes")
+    const tasks = []
 
     for (let i = 1; i <= 30; i++) {
+        tasks.push(`TASK DAY ${i}`)
+    }
+
+    const inputSearch = document.querySelector('.input-search')
+    inputSearch.addEventListener("input", (event) => {
+        const userQuery = removeSpaces(event.target.value)
+        
+        const searchedTask = tasks.filter((task) => {
+            const clearTask = removeSpaces(task)
+            return clearTask.includes(userQuery)
+        })
+        
+        renderTask(searchedTask)
+    })
+
+    renderTask(tasks)
+}
+
+function removeSpaces(str) {
+    strLowercase = str.toLowerCase()
+
+    return strLowercase.replace(/\s/g, "") // remove spaces
+}
+
+function renderTask(tasks) {
+    const routes = document.getElementById("routes")
+    routes.innerHTML = ''
+
+    tasks.forEach((task) => {
+        const taskSplitted = task.split(" ")
+        const taskPath = taskSplitted.join('-').toLowerCase()
+        
         const route = document.createElement("a")
-        route.textContent = `TASK DAY ${i}`
-        route.setAttribute("href", `task-day-${i}`)
+        route.textContent = task
+        route.setAttribute("href", taskPath)
 
         routes.append(route)
-    }
+    })
 }
