6efe323723d4a180df9455d26ff1c9a42f10fe3a
yudistiraardi <mail.ardix@gmail.com>
2024-05-01 17:16:15 +0700

    refactor: implement ORM menthods into route services for task-day-14 project

--- task-day-14/src/scripts/controllers/controllers.js
+++ task-day-14/src/scripts/controllers/controllers.js
@@ -8,13 +8,15 @@ const sequelize = new Sequelize(config.development)
 const controllers = {
     // views
     async homeView(req, res) {
-        const query = `SELECT * FROM projects`
-        const responses = await sequelize.query(query, {
-            type: QueryTypes.SELECT,
-        })
+        // const query = `SELECT * FROM projects`
+        // const responses = await sequelize.query(query, {
+        //     type: QueryTypes.SELECT,
+        // })
+
+        const responses = await projectsModel.findAll()
 
         const projects = responses.map((response) => {
-            return utils.prepareProject(response)
+            return utils.prepareProject(response.dataValues)
         })
 
         res.render('index', { projects })
@@ -23,26 +25,40 @@ const controllers = {
     async editProjectView(req, res) {
         const { id } = req.params
 
-        const query = `SELECT * FROM projects WHERE id = ${id}`
-        const requestedProject = await sequelize.query(query, {
-            type: QueryTypes.SELECT,
+        // const query = `SELECT * FROM projects WHERE id = ${id}`
+        // const requestedProject = await sequelize.query(query, {
+        //     type: QueryTypes.SELECT,
+        // })
+
+        const response = await projectsModel.findOne({
+            where: {
+                id: id,
+            },
+        })
+        const requestedProject = response.dataValues
+
+        requestedProject.technologies.forEach((tech) => {
+            requestedProject[tech] = 'checked'
         })
 
-        requestedProject[0].technologies.forEach((tech) => {
-            requestedProject[0][tech] = 'checked'
-        })
-
-        res.render('edit-project', requestedProject[0])
+        res.render('edit-project', requestedProject)
     },
 
     async detailView(req, res) {
         const { id } = req.params
 
-        const query = `SELECT * FROM projects WHERE id = ${id}`
-        const rawRequestedProject = await sequelize.query(query, {
-            type: QueryTypes.SELECT,
+        // const query = `SELECT * FROM projects WHERE id = ${id}`
+        // const rawRequestedProject = await sequelize.query(query, {
+        //     type: QueryTypes.SELECT,
+        // })
+
+        const response = await projectsModel.findOne({
+            where: {
+                id: id,
+            },
         })
-        const requestedProject = utils.prepareProject(rawRequestedProject[0])
+
+        const requestedProject = utils.prepareProject(response.dataValues)
 
         res.render('detail', requestedProject)
     },
@@ -63,10 +79,20 @@ const controllers = {
     async createProject(req, res) {
         const { name, start, end, summary, description, technologies, image } =
             req.body
-        const strTechnologies = technologies.join(',')
+        // const strTechnologies = technologies.join(',')
 
-        const query = `INSERT INTO projects (name, start, "end", summary, description, technologies, image) VALUES ('${name}', '${start}', '${end}', '${summary}', '${description}', '{${strTechnologies}}', '/assets/images/jennie.jpg')`
-        await sequelize.query(query, { type: QueryTypes.INSERT })
+        // const query = `INSERT INTO projects (name, start, "end", summary, description, technologies, image) VALUES ('${name}', '${start}', '${end}', '${summary}', '${description}', '{${strTechnologies}}', '/assets/images/jennie.jpg')`
+        // await sequelize.query(query, { type: QueryTypes.INSERT })
+
+        await projectsModel.create({
+            name,
+            start,
+            end,
+            summary,
+            description,
+            technologies,
+            image: '/assets/images/jennie.jpg',
+        })
 
         res.send({ status: 'Ok!' })
     },
@@ -74,8 +100,14 @@ const controllers = {
     async deleteProject(req, res) {
         const { id } = req.params
 
-        const query = `DELETE FROM projects WHERE id = ${id}`
-        await sequelize.query(query, { type: QueryTypes.DELETE })
+        // const query = `DELETE FROM projects WHERE id = ${id}`
+        // await sequelize.query(query, { type: QueryTypes.DELETE })
+
+        await projectsModel.destroy({
+            where: {
+                id: id,
+            },
+        })
 
         res.send({ status: 'Ok!' })
     },
@@ -84,10 +116,27 @@ const controllers = {
         const { id } = req.params
         const { name, start, end, summary, description, technologies, image } =
             req.body
-        const strTechnologies = technologies.join(',')
+        // const strTechnologies = technologies.join(',')
 
-        const query = `UPDATE projects SET name = '${name}', start = '${start}', "end" = '${end}', summary = '${summary}', description = '${description}', technologies = '{${strTechnologies}}', image = '/assets/images/jennie.jpg' WHERE id = ${id}`
-        await sequelize.query(query, { type: QueryTypes.UPDATE })
+        // const query = `UPDATE projects SET name = '${name}', start = '${start}', "end" = '${end}', summary = '${summary}', description = '${description}', technologies = '{${strTechnologies}}', image = '/assets/images/jennie.jpg' WHERE id = ${id}`
+        // await sequelize.query(query, { type: QueryTypes.UPDATE })
+
+        await projectsModel.update(
+            {
+                name,
+                start,
+                end,
+                summary,
+                description,
+                technologies,
+                image: '/assets/images/jennie.jpg',
+            },
+            {
+                where: {
+                    id: id,
+                },
+            }
+        )
 
         res.send({ status: 'Ok!' })
     },
