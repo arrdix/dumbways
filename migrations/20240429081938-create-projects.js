d73756f5795190df4da7b030a51e2b989717f0c9
yudistiraardi <mail.ardix@gmail.com>
2024-05-01 16:23:10 +0700

    refactor: change migration & model configs for task-day-14 project

--- migrations/20240429081938-create-projects.js
+++ migrations/20240429081938-create-projects.js
@@ -1,46 +1,48 @@
-'use strict';
+'use strict'
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('projects', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      start_date: {
-        type: Sequelize.DATE
-      },
-      end_date: {
-        type: Sequelize.DATE
-      },
-      summary: {
-        type: Sequelize.STRING
-      },
-      description: {
-        type: Sequelize.TEXT
-      },
-      technologies: {
-        type: Sequelize.ARRAY(Sequelize.STRING)
-      },
-      image: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('projects');
-  }
-};
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('projects', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                type: Sequelize.STRING,
+            },
+            start: {
+                type: Sequelize.DATEONLY,
+            },
+            end: {
+                type: Sequelize.DATEONLY,
+            },
+            summary: {
+                type: Sequelize.STRING,
+            },
+            description: {
+                type: Sequelize.TEXT,
+            },
+            technologies: {
+                type: Sequelize.ARRAY(Sequelize.STRING),
+            },
+            image: {
+                type: Sequelize.STRING,
+            },
+            createdAt: {
+                type: Sequelize.DATEONLY,
+                allowNull: false,
+                defaultValue: Sequelize.fn('now'),
+            },
+            updatedAt: {
+                type: Sequelize.DATEONLY,
+                allowNull: false,
+                defaultValue: Sequelize.fn('now'),
+            },
+        })
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('projects')
+    },
+}
--- models/projects.js
+++ models/projects.js
@@ -1,29 +1,30 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class projects extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class projects extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  projects.init({
-    name: DataTypes.STRING,
-    start_date: DataTypes.DATE,
-    end_date: DataTypes.DATE,
-    summary: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    technologies: DataTypes.ARRAY(DataTypes.STRING),
-    image: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'projects',
-  });
-  return projects;
-};
+    projects.init(
+        {
+            name: DataTypes.STRING,
+            start: DataTypes.DATE,
+            end: DataTypes.DATE,
+            summary: DataTypes.STRING,
+            description: DataTypes.TEXT,
+            technologies: DataTypes.ARRAY(DataTypes.STRING),
+            image: DataTypes.STRING,
+        },
+        {
+            sequelize,
+            modelName: 'projects',
+        }
+    )
+    return projects
+}
