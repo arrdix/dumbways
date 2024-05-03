0336f939a7b8f21058c26c55951418668ac3eec2
yudistiraardi <mail.ardix@gmail.com>
2024-05-03 22:25:44 +0700

    refactor: update project model configs for task-day-16 project

--- models/projects.js
+++ models/projects.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            // define association here
+            projects.belongsTo(models.users, {
+                foreignKey: 'userId',
+                as: 'author',
+            })
         }
     }
     projects.init(
